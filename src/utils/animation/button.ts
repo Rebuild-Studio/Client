f6f440d93fe1f5ff6f076470a379d187c4b8df0b
JW <jeongwoo_lee3@tmax.co.kr>
2023-08-25 15:03:24 +0900

    feat: utils에 style관련 함수 , button 애니메이션 추가

---
+++ src/utils/animation/button.ts
@@ -0,0 +1,57 @@
+import { ButtonAnimationType } from "@/types/style/buttonAnimations";
+import { keyframes } from "styled-components";
+
+const ButtonAnimation = {
+  translate: "translate",
+  bounce: "bounce",
+  shrink: "shrink",
+  enlarge: "enlarge",
+  none: "none",
+} as const satisfies { [key in ButtonAnimationType]: ButtonAnimationType };
+
+const BounceAnimation = keyframes`
+  0% { transform: translateY(0); }
+  50% { transform: translateY(-5px); }
+  100% { transform: translateY(0); }
+`;
+
+const TranslateToRightBottomAnimation = keyframes`
+  0% { transform: translate(0, 0); }
+  50% { transform: translate(3px, 3px); }
+  100% { transform: translate(0, 0); }
+`;
+
+const ShrinkAnimation = keyframes`
+  0% { transform: scale(1); }
+  50% { transform: scale(0.95); }
+  100% { transform: scale(1); }
+`;
+
+const EnlargeAnimation = keyframes`
+  0% { transform: scale(1); }
+  50% { transform: scale(1.05); }
+  100% { transform: scale(1); }
+`;
+
+/**
+ * @description 버튼 클릭시 애니메이션(keyframe)을 반환합니다.
+ * @param {ButtonAnimationType} clickAnimation
+ * @returns keyframes
+ */
+const getButtonClickAnimation = (clickAnimation: ButtonAnimationType) => {
+  switch (clickAnimation) {
+    case ButtonAnimation.translate:
+      return TranslateToRightBottomAnimation;
+    case ButtonAnimation.bounce:
+      return BounceAnimation;
+    case ButtonAnimation.shrink:
+      return ShrinkAnimation;
+    case ButtonAnimation.enlarge:
+      return EnlargeAnimation;
+    case ButtonAnimation.none:
+      return "";
+  }
+};
+
+export { getButtonClickAnimation };
+export type { ButtonAnimation };
---
+++ src/utils/style/getShadow.ts
@@ -0,0 +1,9 @@
+import { shadows } from "@/resources/shadows/shadow";
+import { CSSHexColor } from "@/types/style/cssUnits";
+import { CSSShadow, ShadowType } from "@/types/style/shadow";
+
+const getShadow = (hex: CSSHexColor, type: ShadowType): CSSShadow => {
+  return shadows[type](hex);
+};
+
+export { getShadow };
