e37a7159a63d9f193911a08fd6dce400bc9c8f99
JW <jeongwoo_lee3@tmax.co.kr>
2023-08-25 14:57:02 +0900

    design: font, shadow resource 추가

---
+++ src/resources/shadows/shadow.ts
@@ -0,0 +1,11 @@
+import { Shadows } from "@/types/style/shadow";
+import { hexToRgba } from "@/utils/style/hexToRgb";
+
+const shadows = {
+  default: (hex) => `2.5px 2.5px 2.5px 0px ${hexToRgba(hex, 0.5)}`,
+  elevated: (hex) => `0px 5px 0px 0px ${hexToRgba(hex, 0.5)}`,
+  pressed: (hex) => `0px 0px 0px 0px ${hexToRgba(hex, 0.5)}`,
+  none: () => "none",
+} as const satisfies Shadows;
+
+export { shadows };
