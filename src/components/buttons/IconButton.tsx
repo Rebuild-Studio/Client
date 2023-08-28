7984ea6caee8c8089be59614fecc5c6a2d6a6aa0
JW <jeongwoo_lee3@tmax.co.kr>
2023-08-28 15:09:38 +0900

    fix: 안쓰는 import 삭제

--- src/components/buttons/IconButton.tsx
+++ src/components/buttons/IconButton.tsx
@@ -3,7 +3,6 @@ import { Props as ButtonProps, SCButton } from "../common/Button";
 import { basicColors, bgColors } from "@/resources/colors/colors";
 import { styled } from "styled-components";
 import { getShadow } from "@/utils/style/getShadow";
-import { hover } from "node_modules/@testing-library/user-event/dist/types/convenience";
 
 const SCIconButton = styled(SCButton)`
   display: flex;
