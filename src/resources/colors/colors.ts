60f3b11ae1a9e04c32d3d227521ab671eb10f5ae
JW <jeongwoo_lee3@tmax.co.kr>
2023-08-29 17:42:22 +0900

    feat: 컬러추가

--- src/resources/colors/colors.ts
+++ src/resources/colors/colors.ts
@@ -1,11 +1,7 @@
-import { CSSHexColor } from "@/types/style/CssUnits";
 /**
  * 사용하는 색상을 명세하는 곳 입니다.
  * 새로 추가되는 색상을 여기에 추가하시고 import해서 사용하시면 됩니다.
  */
-type HexColorObject = {
-  [key: string]: CSSHexColor;
-};
 
 import { CSSHexColor } from "@/types/style/CssUnits";
 
@@ -21,6 +17,8 @@ const basicColors = {
 const bgColors = {
   "222222": "#222222",
   "101728": "#101728",
+  "282828": "#282828",
+  "1c1c1c": "#1c1c1c",
 } as const satisfies HexColorObject;
 
 const grayColors = {
