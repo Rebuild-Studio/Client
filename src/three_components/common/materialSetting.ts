62e06c5cdd667ef9715a80f212820a299ddcd3be
jeonghwan <jeonghwan9589@gmail.com>
2023-09-05 10:52:14 +0900

    feat/threeComponents
    1. materialSetting 생성자 변경

--- src/three_components/common/materialSetting.ts
+++ src/three_components/common/materialSetting.ts
@@ -3,10 +3,10 @@ import * as THREE from "three";
 const getDefaultMaterialSetting = () => {
   const material = new THREE.MeshPhysicalMaterial({
     color: 0xbbbbbb,
+    metalness: 0,
+    roughness: 0.5,
+    ior: 1.45,
   });
-  material.metalness = 0;
-  material.roughness = 0.5;
-  material.ior = 1.45;
 
   return material;
 };
