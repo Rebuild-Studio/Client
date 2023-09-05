e1d1ea246c411e6c6ff4837e7bb248ecda758898
JW <jeongwoo_lee3@tmax.co.kr>
2023-09-05 14:57:25 +0900

    feat: Grid 컴포넌트 분리, Canvas style따로 빼기

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,8 +1,8 @@
-import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import Grid from "./grid";
 import styled from "styled-components";
-import { gridColor, basicColors, bgColors } from "@/resources/colors/colors";
+import { basicColors, bgColors } from "@/resources/colors/colors";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -17,93 +17,17 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-const createGrid = () => {
-  const axisLength = 25;
-  const xAxisGeometry = new Float32Array([axisLength, 0, 0, -axisLength, 0, 0]);
-  const yAxisGeometry = new Float32Array([0, 0, axisLength, 0, 0, -axisLength]);
+const CustomCanvas = styled(Canvas)`
+  width: "100%";
+  height: "100%";
+  background-color: ${bgColors.sceneBackground};
+`;
 
-  // Create BufferGeometry instances using the defined BufferAttributes
-  const xAxisBufferGeometry = new THREE.BufferGeometry();
-  const yAxisBufferGeometry = new THREE.BufferGeometry();
-
-  // Set the position attribute using setAttribute
-  xAxisBufferGeometry.setAttribute(
-    "position",
-    new THREE.BufferAttribute(xAxisGeometry, 3)
-  );
-  yAxisBufferGeometry.setAttribute(
-    "position",
-    new THREE.BufferAttribute(yAxisGeometry, 3)
-  );
-  return (
-    <>
-      <group>
-        <mesh position={[0, 0, 0]}>
-          <gridHelper args={[50, 50, , gridColor.gray]} />
-          <gridHelper args={[50, 10, , gridColor.black]} />
-        </mesh>
-
-        <group>
-          <mesh position={[0, 0, 0]}>
-            <line>
-              <bufferGeometry attach="geometry" {...xAxisBufferGeometry}>
-                <bufferAttribute
-                  attach="position"
-                  count={2}
-                  array={
-                    new Float32Array([axisLength, 0, 0, -axisLength, 0, 0])
-                  }
-                  itemSize={3}
-                />
-              </bufferGeometry>
-              <lineBasicMaterial
-                attach="material"
-                color={gridColor.Xaxis}
-                fog={false}
-                toneMapped={false}
-                transparent={true}
-              />
-            </line>
-
-            {/* y axis */}
-            <line>
-              <bufferGeometry attach="geometry" {...yAxisBufferGeometry}>
-                <bufferAttribute
-                  attach="position"
-                  count={2}
-                  array={
-                    new Float32Array([0, 0, axisLength, 0, 0, -axisLength])
-                  }
-                  itemSize={3}
-                />
-              </bufferGeometry>
-              <lineBasicMaterial
-                attach="material"
-                color={gridColor.Yaxis}
-                fog={false}
-                toneMapped={false}
-                transparent={true}
-              />
-            </line>
-          </mesh>
-        </group>
-      </group>
-    </>
-  );
-};
-
-function Scene() {
+const Scene = () => {
   return (
     <Wrapper>
       <Container>
-        <Canvas
-          style={{
-            width: "100%",
-            height: "100%",
-            backgroundColor: bgColors.sceneBackground,
-          }}
-          camera={{ fov: 50, position: [0, 2, 3.0] }}
-        >
+        <CustomCanvas camera={{ fov: 50, position: [0, 2, 3.0] }}>
           <ambientLight
             intensity={0.3}
             visible={true}
@@ -123,12 +47,12 @@ function Scene() {
             shadow-camera-top={30}
             shadow-camera-bottom={-30}
           ></directionalLight>
-          {createGrid()}
+          <Grid />
           <OrbitControls enableDamping={false} />
-        </Canvas>
+        </CustomCanvas>
       </Container>
     </Wrapper>
   );
-}
+};
 
 export default Scene;
