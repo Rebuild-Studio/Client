1b8c5a334f8491ad7c3adbc03de855413d389141
JW <jeongwoo_lee3@tmax.co.kr>
2023-09-22 18:44:03 +0900

    feat : App 에 RightPanel 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import TopBar from "./components/TopBar";
+import RightPanel from "./components/common/RightPanel/RightPanel";
 import AppWrapper from "./components/layout/wrapper/AppWrapper";
 import Scene from "./three_components/common/Scene";
-
 const App = () => {
   return (
     <AppWrapper>
       <TopBar />
+      <RightPanel />
       <Scene />
     </AppWrapper>
   );
