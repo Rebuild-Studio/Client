9668c18432947b1452c98157bbefa27f8ebf0aac
junman <junman9595@gmail.com>
2023-09-26 14:43:03 +0900

    feat: 파일 이름 변경

---
+++ src/stories/assetLibrary/Body.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryFn, StoryObj } from "@storybook/react";
+import { basicColors } from "@/resources/colors/colors";
+import Body from "@/features/assetLibrary/body";
+
+const meta = {
+  component: Body,
+  title: "Feature/AssetLibrary/AssetLibraryBody",
+  tags: ["autodocs"],
+  argTypes: {},
+  parameters: {
+    docs: {
+      description: {
+        component: "에셋 라이브러리 바디입니다.",
+      },
+    },
+  },
+} satisfies Meta<typeof Body>;
+
+export default meta;
+type Story = StoryObj<typeof Body>;
+
+export const AssetLibraryBody = {
+  decorators: [
+    (Story: StoryFn) => (
+      <div style={{ backgroundColor: basicColors.black }}>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Story;
