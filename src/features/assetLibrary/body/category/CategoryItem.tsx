04607f0a41eb616973c3993c5a2464b782631d8d
junman <junman9595@gmail.com>
2023-09-26 16:04:35 +0900

    category item with icon

--- src/features/assetLibrary/body/category/CategoryItem.tsx
+++ src/features/assetLibrary/body/category/CategoryItem.tsx
@@ -1,4 +1,6 @@
 import { MainCategoryType } from "@/features/constants/mainCategory";
+import { bgColors } from "@/resources/colors/colors";
+import { styled } from "styled-components";
 
 interface Props {
   name: string;
@@ -6,8 +8,32 @@ interface Props {
   type?: "main" | "sub";
 }
 
+const ItemContainer = styled.button`
+  height: 38px;
+  display: flex;
+  align-items: center;
+  transition: background-color 0.2s ease-in-out;
+  padding: 10px;
+  box-sizing: border-box;
+
+  &:hover {
+    background-color: ${bgColors[343434]};
+  }
+`;
+
+const ItemText = styled.span`
+  margin-left: 10px;
+  font-size: 14px;
+`;
+
 const CategoryItem = ({ name, category, type }: Props) => {
-  return <button>{name + category + type}</button>;
+  const iconUrl = `/icons/assetLibrary/category/${type}/${category}.svg`;
+  return (
+    <ItemContainer>
+      <img src={iconUrl} alt={name} />
+      <ItemText>{name}</ItemText>
+    </ItemContainer>
+  );
 };
 
 export default CategoryItem;
