08c3dbf5ed67dca9fcbdec058245cc21a949de70
donghwan_park <donghwan_park@tmax.co.kr>
2023-09-26 14:46:24 +0900

    fix : 카드 컴포넌트 여러 props 받을 수 있도록 수정

---
+++ src/stories/Card.stories.ts
@@ -0,0 +1,37 @@
+import { Card } from "@/components/common/Card";
+import { bgColors } from "@/resources/colors/colors";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  component: Card,
+  title: "Component/Card",
+  tags: ["autodocs"],
+  parameters: {
+    backgrounds: {
+      default: "my-custom-background",
+      values: [
+        {
+          name: "my-custom-background",
+          value: bgColors[282828],
+        },
+      ],
+    },
+  },
+} satisfies Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof Card>;
+
+export const Example = {
+  args: {
+    name: "오피스 테마",
+    width: "245px",
+    height: "245px",
+    imageWidth: "100px",
+    imageHeight: "100px",
+    thumbnail: "/example/StartScene_Office.png",
+    border: "1px dotted #fff",
+    alt: "error",
+    isClicked: false,
+  },
+} satisfies Story;
