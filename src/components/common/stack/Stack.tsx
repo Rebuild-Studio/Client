373064d25666c9fe1d796bd0a0d6cc7562531015
junman <junman9595@gmail.com>
2023-09-27 13:42:27 +0900

    feat: layout에 기존 mx dashboard stack 추가

---
+++ src/components/common/stack/Stack.tsx
@@ -0,0 +1,50 @@
+import { grayColors } from "@/resources/colors/colors";
+import { CSSColor, CSSSize } from "@/types/style/CssUnits";
+import { styled } from "styled-components";
+
+interface ContainerProps {
+  $width: Props["width"];
+  $height: Props["height"];
+  $border: Props["border"];
+}
+
+const Container = styled.div<ContainerProps>`
+  display: flex;
+  flex-direction: column;
+  border: ${({ $border }) => $border || `1px solid ${grayColors.E2E2E2}`};
+  width: ${({ $width }) => $width};
+  height: ${({ $height }) => $height};
+`;
+
+interface Props {
+  className?: string;
+  role?: string;
+  hoverBackgroundColor?: CSSColor;
+  height?: CSSSize;
+  width?: CSSSize;
+  border?: string;
+  children?: React.ReactNode;
+}
+
+const Stack = ({
+  className,
+  role = "stack",
+  children,
+  width = "fit-content",
+  height = "fit-content",
+  border,
+}: Props) => {
+  return (
+    <Container
+      className={className}
+      role={role}
+      $width={width}
+      $height={height}
+      $border={border}
+    >
+      {children}
+    </Container>
+  );
+};
+
+export default Stack;
---
+++ src/stories/layout/stack/StackItem.stories.tsx
@@ -0,0 +1,30 @@
+import StackItem from "@/components/common/stack/StackItem";
+import { grayColors } from "@/resources/colors/colors";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  component: StackItem,
+  title: "Layout/StackItem",
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof StackItem>;
+
+export default meta;
+type Story = StoryObj<typeof StackItem>;
+
+export const Default = {
+  args: {
+    label: "옵션1",
+    hoverBackgroundColor: grayColors.lightGray,
+  },
+} satisfies Story;
+
+export const StackItemWithImage = {
+  args: {
+    label: "옵션1",
+    hoverBackgroundColor: grayColors.lightGray,
+  },
+  render: (args) => {
+    return <StackItem {...args}>{/* <Icon /> */}</StackItem>;
+  },
+} satisfies Story;
