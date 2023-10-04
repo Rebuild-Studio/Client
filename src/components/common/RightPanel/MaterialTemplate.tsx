d7b390efdb9c4a5ddb89c6ecf1fed0b4a0ce8183
JW <jeongwoo_lee3@tmax.co.kr>
2023-10-04 14:28:40 +0900

    feat: Material 스토어를 primitive 스토어에서 분리

--- src/components/common/RightPanel/MaterialTemplate.tsx
+++ src/components/common/RightPanel/MaterialTemplate.tsx
@@ -1,8 +1,7 @@
 import { observer } from "mobx-react";
 import styled from "styled-components";
 import Grid from "../Grid";
-import data_store from "./MaterialData";
-
+import dataStore from "./MaterialData";
 import storeContainer from "@/store/storeContainer";
 
 const ImageStyled = styled.img<{ isSelected: boolean }>`
@@ -13,18 +12,18 @@ const ImageStyled = styled.img<{ isSelected: boolean }>`
 `;
 
 const MaterialTemplate = observer(() => {
-  const { primitiveStore } = storeContainer;
+  const { selectedObjectStore } = storeContainer;
 
   return (
     <Grid
-      items={data_store.mat_tex_list.map((template, index) => (
+      item={dataStore.materialTextureList.map((template, index) => (
         <ImageStyled
           key={index}
           isSelected={true}
-          src={"/icons/rightTab/" + template[1] + ".png"}
+          src={`/icons/rightTab/${template[1]}.png`}
           alt={`item-${index}`}
           onClick={() => {
-            primitiveStore.setSelectedMaterial(template[1]);
+            selectedObjectStore.setSelectedMaterial(template[1]);
           }}
         />
       ))}
