31319e520a17e09cd68040d2abe348c020be5966
JW <jeongwoo_lee3@tmax.co.kr>
2023-10-04 14:27:32 +0900

    fix: 머테리얼 금속성, 거칠기를 슬라이더에 각각의 onChange 함수 전달

--- src/components/common/RightPanel/MaterialInfo.tsx
+++ src/components/common/RightPanel/MaterialInfo.tsx
@@ -2,10 +2,16 @@ import styled from "styled-components";
 import Slider from "../Slider";
 import CustomMenu from "@/components/layout/Menu";
 import MaterialTemplate from "./MaterialTemplate";
+import ColorPicker from "./ColorPicker";
+import * as THREE from "three";
+import { HsvaColor } from "@uiw/color-convert";
+import { useEffect, useState } from "react";
+import storeContainer from "@/store/storeContainer";
 
 interface MaterialInfoProps {
   metalness: number;
   roughness: number;
+  color: HsvaColor;
 }
 const Wrapper = styled.div`
   margin-top: 10px;
@@ -21,7 +27,35 @@ const TitleWrapper = styled.div`
   justify-content: space-between;
   font-size: 10px;
 `;
-const Material = ({ metalness = 1, roughness = 0.5 }: MaterialInfoProps) => {
+
+const Material = ({
+  metalness = 1,
+  roughness = 0.5,
+  color,
+}: MaterialInfoProps) => {
+  // const hsvaColor = rgbaToHsva(color);
+
+  const [mesh, setMesh] = useState(new THREE.Mesh());
+  const { primitiveStore } = storeContainer;
+  const keys = Object.keys(primitiveStore.selectedPrimitives);
+  useEffect(() => {
+    if (primitiveStore.selectedPrimitives[keys[0]]) {
+      setMesh(primitiveStore.selectedPrimitives[keys[0]]);
+    }
+  }, [primitiveStore.selectedPrimitives]);
+
+  const onMetalnessChange = (newValue: number) => {
+    if (mesh.material instanceof THREE.MeshStandardMaterial) {
+      const material = mesh.material;
+      material.metalness = newValue;
+    }
+  };
+  const onRoughnessChange = (newValue: number) => {
+    if (mesh.material instanceof THREE.MeshStandardMaterial) {
+      const material = mesh.material;
+      material.roughness = newValue;
+    }
+  };
   return (
     <>
       <Wrapper>
@@ -31,8 +65,8 @@ const Material = ({ metalness = 1, roughness = 0.5 }: MaterialInfoProps) => {
             <CustomMenu title={"머테리얼"} MenuItem={<MaterialTemplate />} />
           </TitleWrapper>
           <TitleWrapper>
-            <span>{"기본"}</span>
-            <CustomMenu title={"머테리얼"} MenuItem={[<span>하이</span>]} />
+            <span>{"기본 컬러"}</span>
+            <ColorPicker label={"기본 컬러"} color={color} />
           </TitleWrapper>
         </MaterialMenu>
         <Slider
@@ -41,12 +75,14 @@ const Material = ({ metalness = 1, roughness = 0.5 }: MaterialInfoProps) => {
           step={0.01}
           initValue={metalness}
           title="금속성"
+          onMaterialChange={onMetalnessChange}
         />
         <Slider
           min={0}
           max={1}
           step={0.01}
           initValue={roughness}
+          onMaterialChange={onRoughnessChange}
           title="거칠기"
         />
       </Wrapper>
