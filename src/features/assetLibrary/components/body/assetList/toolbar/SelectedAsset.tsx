7d82aacb12ac3db727e7e79bb6e380759d366218
junman <junman9595@gmail.com>
2023-10-06 13:19:21 +0900

    feat: selected asset에 대한 삭제 및 디스플레이 버튼 생성, 스토리 추가

---
+++ src/features/assetLibrary/components/body/assetList/toolbar/SelectedAsset.tsx
@@ -0,0 +1,42 @@
+import useControlSelectedItem from "@/features/assetLibrary/hooks/useControlSelectedItem";
+import { LibraryAsset } from "@/features/assetLibrary/types/fetchAssetType";
+import { basicColors, grayColors } from "@/resources/colors/colors";
+import styled from "styled-components";
+
+const DeselectButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+
+  height: 32px;
+
+  padding: 0px 8px;
+  box-sizing: border-box;
+
+  border: 1px solid ${grayColors.panelGray};
+  border-radius: 5px;
+
+  color: ${basicColors.white};
+  background-color: ${basicColors.black};
+
+  font-weight: 600;
+`;
+
+const CloseIcon = () => <img src="/icons/common/Close.svg" alt="close" />;
+
+interface Props {
+  asset: LibraryAsset;
+}
+
+const SelectedAsset = ({ asset }: Props) => {
+  const [, updateSelectedAsset] = useControlSelectedItem(asset);
+  return (
+    <DeselectButton onClick={updateSelectedAsset}>
+      <span>{asset.name}</span>
+      <CloseIcon />
+    </DeselectButton>
+  );
+};
+
+export default SelectedAsset;
