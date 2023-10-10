522ff2eaea6e0d525d079020f44830a09c06631f
junman <junman9595@gmail.com>
2023-10-10 16:12:55 +0900

    feat: 쓰로틀, 인피니트 스크롤 코드 추가 및 타입 지정

---
+++ src/utils/infiniteScroll/infiniteScroll.ts
@@ -0,0 +1,28 @@
+import React from "react";
+
+/**
+ *
+ * @param {ref} container - ref
+ * @param {number} page - 현재 페이지
+ * @param {function} setPage - 현재 페이지 변경 함수
+ * @param {number} offset - 무한 스크롤 offset(기본값 0)
+ * @description - 무한 스크롤
+ */
+const infiniteScroll = <T extends HTMLElement>(
+  ref: React.RefObject<T>,
+  page: number,
+  setPage: React.Dispatch<React.SetStateAction<number>>,
+  offset = 0
+) => {
+  if (
+    ref &&
+    ref.current &&
+    ref.current.scrollHeight - ref.current.scrollTop <=
+      ref.current.clientHeight + offset
+  ) {
+    console.log(page);
+    setPage(page + 1);
+  }
+};
+
+export default infiniteScroll;
