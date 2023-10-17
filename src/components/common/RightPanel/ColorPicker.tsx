06576ba965875b1c4e9398aaf389b83276049419
junman <junman9595@gmail.com>
2023-10-17 16:40:38 +0900

    fix: 불필요 import 제거

--- src/components/common/RightPanel/ColorPicker.tsx
+++ src/components/common/RightPanel/ColorPicker.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { observer } from "mobx-react";
 import styled from "styled-components";
 import { grayColors } from "@/resources/colors/colors";
