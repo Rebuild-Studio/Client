054a62f48e00e562c255607d4fcf28597587ba6c
Jihoo Kim <jihoo94@gmail.com>
2023-10-18 17:04:27 +0900

    refactor: 불필요한 console.log 제거

--- src/features/controllerBar/components/focus/ObjectFocus.tsx
+++ src/features/controllerBar/components/focus/ObjectFocus.tsx
@@ -4,7 +4,6 @@ import FocusIcon from "@/features/controllerBar/components/icons/FocusIcon";
 const ObjectFocus = () => {
   const handleObjectFocus = () => {
     // TODO: 오브젝트 포커스 기능 추가
-    console.log("오브젝트 포커스");
   };
 
   return <ControlButton icon={<FocusIcon />} onClick={handleObjectFocus} />;
