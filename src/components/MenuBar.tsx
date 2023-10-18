992a4b46df3f71233b5a9161dad81ba5c0815008
junman <junman9595@gmail.com>
2023-10-18 15:40:57 +0900

    fix:build issue

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -18,7 +18,7 @@ const Menu = ({ label }: { label: string }) => (
     color={basicColors.white}
     fontSize="small"
     label={label}
-    onClick={() => {}}
+    onClick={() => { }}
   />
 );
 const ComponentBtn = () => <Menu label="컴포넌트" />;
@@ -67,29 +67,29 @@ const MenuBar = () => {
     {
       label: "GLB로 내보내기(선택)",
       disabled: true,
-      onClick: () => {},
+      onClick: () => { },
     },
     {
       label: "GLB로 내보내기(전체)",
       disabled: true,
-      onClick: () => {},
+      onClick: () => { },
     },
   ];
   const configureData: MenuItemType[] = [
     {
       label: "인터페이스",
       disabled: false,
-      onClick: () => {},
+      onClick: () => { },
     },
     {
       label: "단축키",
       disabled: false,
-      onClick: () => {},
+      onClick: () => { },
     },
     {
       label: "자동저장",
       disabled: true,
-      onClick: () => {},
+      onClick: () => { },
     },
   ];
 
@@ -97,22 +97,22 @@ const MenuBar = () => {
     {
       label: "사용 안내서",
       disabled: false,
-      onClick: () => {},
+      onClick: () => { },
     },
     {
       label: "새로운 기능",
       disabled: true,
-      onClick: () => {},
+      onClick: () => { },
     },
     {
       label: "오류 보고/개선 제안",
       disabled: false,
-      onClick: () => {},
+      onClick: () => { },
     },
     {
       label: "Copyright 2023 TmaxMetaverse",
       disabled: true,
-      onClick: () => {},
+      onClick: () => { },
     },
   ];
 
@@ -145,14 +145,6 @@ const MenuBar = () => {
 };
 
 export default MenuBar;
-
-const StyledLogo = styled.img`
-  margin-left: 10px;
-`;
-const ButtonWrapper = styled.div`
-  margin-left: auto;
-  margin-right: 10px;
-`;
 
 const StyledBar = styled.div`
   display: flex;
