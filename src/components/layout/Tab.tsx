569b91023aedb0e0e97febd019233fadaaed7e04
JW <jeongwoo_lee3@tmax.co.kr>
2023-10-19 12:54:40 +0900

    fix : tab과 tabpanel 분리

--- src/components/layout/Tab.tsx
+++ src/components/layout/Tab.tsx
@@ -1,33 +1,27 @@
-import React, { useState } from "react";
 import styled from "styled-components";
-import { basicColors } from "@/resources/colors/colors";
+import { basicColors, grayColors } from "@/resources/colors/colors";
 
 interface TabProps {
   tabs: string[];
-  tabContents: React.ReactNode[];
+  activeTab: number;
+  onTabChange: (index: number) => void;
 }
 
-const Tab = ({ tabs, tabContents }: TabProps) => {
-  const [activeTab, setActiveTab] = useState(0);
-  const handleTabClick = (index: number) => {
-    setActiveTab(index);
-  };
-
+const Tab = ({ tabs, activeTab, onTabChange }: TabProps) => {
   return (
     <Wrapper>
       <TabsContainer>
         {tabs.map((tab, index) => (
           <TabButton
             key={index}
-            isActive={index === activeTab}
-            onClick={() => handleTabClick(index)}
+            $isActive={index === activeTab}
+            onClick={() => onTabChange(index)}
           >
             {tab}
           </TabButton>
         ))}
       </TabsContainer>
-      <TabIndicator activeTab={activeTab} numberOfTabs={tabs.length} />
-      <TabContent>{tabContents[activeTab]}</TabContent>
+      <TabIndicator $activeTab={activeTab} $numberOfTabs={tabs.length} />
     </Wrapper>
   );
 };
@@ -46,10 +40,14 @@ const TabsContainer = styled.div`
   border-bottom: 2px solid ${basicColors.black};
 `;
 
-const TabButton = styled.button<{ isActive: boolean }>`
+const TabButton = styled.button<{ $isActive: boolean }>`
   width: 100%;
   padding: 10px;
-  color: ${basicColors.white};
+  color: ${({ $isActive }) =>
+    $isActive ? basicColors.white : grayColors.panelGray};
+  font-family: SpoqaHanSansNeo;
+  font-size: 13px;
+  font-weight: 500;
   border: none;
   cursor: pointer;
   outline: none;
@@ -57,14 +55,10 @@ const TabButton = styled.button<{ isActive: boolean }>`
   transition: background-color 0.3s ease-in-out, color 0.3s ease-in-out;
 `;
 
-const TabIndicator = styled.div<{ activeTab: number; numberOfTabs: number }>`
+const TabIndicator = styled.div<{ $activeTab: number; $numberOfTabs: number }>`
   height: 2px;
-  background-color: ${basicColors.primary};
-  width: calc(100% / (${(props) => props.numberOfTabs}));
-  transform: translateX(${(props) => `calc(${props.activeTab * 100}%)`});
+  background-color: ${basicColors.white};
+  width: calc(100% / (${(props) => props.$numberOfTabs}));
+  transform: translateX(${(props) => `calc(${props.$activeTab * 100}%)`});
   transition: transform 0.3s;
-`;
-
-const TabContent = styled.div`
-  padding: 10px;
 `;
