4e5daa932f653236e09fb6fda51e286fa6619ba7
okorion <ok.or.orion@gmail.com>
2023-10-19 17:17:43 +0900

    fix: storybook 빌드 오류떠서 임시로 주석 처리

--- src/stories/componentList/ExampleCards.stories.ts
+++ src/stories/componentList/ExampleCards.stories.ts
@@ -1,28 +1,28 @@
-import { ExampleCards } from "@/features/projectList/components/TemplateCards";
-import type { Meta, StoryObj } from "@storybook/react";
+// import { ExampleCards } from "@/features/projectList/components/TemplateCards";
+// import type { Meta, StoryObj } from "@storybook/react";
 
-const meta = {
-  component: ExampleCards,
-  title: "Component/Layout/ComponentList/ExampleCards",
-  tags: ["autodocs"],
-} satisfies Meta<typeof ExampleCards>;
+// const meta = {
+//   component: ExampleCards,
+//   title: "Component/Layout/ComponentList/ExampleCards",
+//   tags: ["autodocs"],
+// } satisfies Meta<typeof ExampleCards>;
 
-export default meta;
-type Story = StoryObj<typeof ExampleCards>;
+// export default meta;
+// type Story = StoryObj<typeof ExampleCards>;
 
-export const basic = {
-  args: {
-    componentData: [
-      {
-        name: "오피스 테마",
-        thumbnail: "/example/StartScene_Office.png",
-        isClicked: false,
-      },
-      {
-        name: "플래피 캣 게임",
-        thumbnail: "/example/game_end.png",
-        isClicked: false,
-      },
-    ],
-  },
-} satisfies Story;
+// export const basic = {
+//   args: {
+//     componentData: [
+//       {
+//         name: "오피스 테마",
+//         thumbnail: "/example/StartScene_Office.png",
+//         isClicked: false,
+//       },
+//       {
+//         name: "플래피 캣 게임",
+//         thumbnail: "/example/game_end.png",
+//         isClicked: false,
+//       },
+//     ],
+//   },
+// } satisfies Story;
