fc366545171a1ea5581519983d89557f25fbafdc
Jihoo Kim <jihoo94@gmail.com>
2023-10-20 14:33:52 +0900

    fix: 영 -> 한 번역

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import Tools from "@components/layout/Tools";
 import editorModeStore from "@store/editorModeStore.ts";
 import InteractionTopBar from "@/interaction(legacyJS)/src/Components/views/00. Common/TopBar_V";
 import InteractionPanel from "@/interaction(legacyJS)/src/Components/views/02. Studio/04. EventPanel/InteractionPanel.jsx";
+import { I18nextProvider } from "react-i18next";
+import i18n from "@/interaction(legacyJS)/src/locale/i18n.js";
 
 const App = observer(() => {
   const { projectStateStore } = storeContainer;
@@ -30,8 +32,10 @@ const App = observer(() => {
             <Tools />
           </EditorWrapper>
           <EditorWrapper $visible={editorMode === "interaction"}>
-            {interactionBarOpen && <InteractionTopBar />}
-            <InteractionPanel />
+            <I18nextProvider i18n={i18n}>
+              {interactionBarOpen && <InteractionTopBar />}
+              <InteractionPanel />
+            </I18nextProvider>
           </EditorWrapper>
         </Main>
       </AppWrapper>
