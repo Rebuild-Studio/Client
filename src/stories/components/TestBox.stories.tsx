92e3b6cc57db7f237cd1f236c2d34545f0fe1be0
Jihoo Kim <jihoo94@gmail.com>
2023-10-24 17:46:52 +0900

    TestBox 스토리 추가

---
+++ src/stories/components/TestBox.stories.tsx
@@ -0,0 +1,18 @@
+import { Card } from "@/components/common/Card";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const TestBox = () => {
+  return <div>test box!</div>;
+};
+
+const meta = {
+  component: TestBox,
+  title: "Component/TestBox",
+} satisfies Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof Card>;
+
+export const Example = {
+  args: {},
+} satisfies Story;
