944852f1363971430e31d45624bfd7efbce48232
jeonghwan <jeonghwan9589@gmail.com>
2023-10-25 14:43:50 +0900

    feat/lottie_component
    1. Modal backdrop 클릭 시 선택적으로 모달 창 제거 가능하도록 기능 추가
    2. Lottie 모달에서 띄우는 함수 추가

--- src/store/projectStateStore.ts
+++ src/store/projectStateStore.ts
@@ -10,12 +10,14 @@ interface ProjectStateProps {
   currentCopyPrimitive: MeshType; // 복사한 prmitive storeId
   canGrouping: boolean; // 다중 선택인 경우 true
   isModalOpened: boolean;
+  isModalCancelable: boolean;
   modalComponent: JSX.Element | null;
   updateDisplay: (type: DisplayType) => void;
   updateGridVisible: (state: GridVisibleType) => void;
   updateCurrentCopyPrimitive: (copyMeshes: MeshType) => void;
   updateCanGrouping: (state: boolean) => void;
   updateModalState: (isOpened: boolean) => void;
+  updateModalCancelable: (isCancelable: boolean) => void;
   updateModalComponent: (component: JSX.Element) => void;
   clearModal: () => void;
 }
@@ -26,6 +28,7 @@ const projectStateStore = observable<ProjectStateProps>({
   currentCopyPrimitive: {},
   canGrouping: false,
   isModalOpened: false,
+  isModalCancelable: true,
   modalComponent: null,
   updateDisplay(type) {
     this.currentDisplay = type;
@@ -45,11 +48,15 @@ const projectStateStore = observable<ProjectStateProps>({
   updateModalComponent(component) {
     this.modalComponent = component;
   },
+  updateModalCancelable(isCancelable: boolean) {
+    this.isModalCancelable = isCancelable;
+  },
   clearModal() {
     this.isModalOpened = false;
+    this.isModalCancelable = true;
     this.modalComponent = null;
   },
 });
 
-export type { ProjectStateProps }
+export type { ProjectStateProps };
 export default projectStateStore;
