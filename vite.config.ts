70d9144b24fcf978961afebe207b34c9d67922d1
sunny <dance@kakao.com>
2023-10-25 14:43:12 +0900

    feat: devtool에서 스타일드 컴포넌트의 클래스이름이 보이도록 기능 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,21 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            "babel-plugin-styled-components",
+            {
+              displayName: true,
+              fileName: false,
+            },
+          ],
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [
       { find: "@", replacement: "/src" },
