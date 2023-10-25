dec5c8995e4539953f862e560b8d2888ed497aa7
jeonghwan <jeonghwan9589@gmail.com>
2023-10-25 14:42:35 +0900

    feat/lottie_component
    1. Lottie Component 제작 및 lottie 파일 추가

---
+++ src/components/common/lottie/MXLottie.tsx
@@ -0,0 +1,26 @@
+import { useLottie } from "lottie-react";
+interface MXLottieSizeProps {
+  width: number;
+  height: number;
+}
+
+interface MXLottieProps {
+  lottieJson: any;
+  loop: boolean;
+  style?: MXLottieSizeProps;
+}
+
+const MXLottie = (props: MXLottieProps) => {
+  const options = {
+    animationData: props.lottieJson,
+    loop: props.loop,
+    style: props.style,
+  };
+
+  const { View } = useLottie(options);
+
+  return <>{View}</>;
+};
+
+export type { MXLottieSizeProps };
+export default MXLottie;
