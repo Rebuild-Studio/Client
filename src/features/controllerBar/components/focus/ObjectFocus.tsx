5a0b4b8773c0f85ed0172b99a65b54161f57e1f8
Jihoo Kim <jihoo94@gmail.com>
2023-10-30 15:14:29 +0900

    feat: 오브젝트 포커스 기능

--- src/features/controllerBar/components/focus/ObjectFocus.tsx
+++ src/features/controllerBar/components/focus/ObjectFocus.tsx
@@ -1,12 +1,34 @@
+import { PerspectiveCamera } from 'three';
+import { observer } from 'mobx-react';
 import ControlButton from '@/features/controllerBar/components/ControlButton';
 import FocusIcon from '@/features/controllerBar/components/icons/FocusIcon';
+import FocusController from '@/features/controllerBar/utils/focus';
+import primitiveStore from '@store/primitive.store';
+import renderStore from '@store/render.store';
 
 const ObjectFocus = () => {
+  const { selectedPrimitives } = primitiveStore;
+  const { controls } = renderStore;
+
   const handleObjectFocus = () => {
-    // TODO: 오브젝트 포커스 기능 추가
+    if (!controls) return;
+
+    const objects = Object.values(selectedPrimitives);
+
+    if (objects.length === 0) {
+      // 선택된 오브젝트가 없을 시 전체 씬 한눈에 보이게
+      void controls.setLookAt(0, 25, 50, 0, 0, 0);
+    } else {
+      const focusController = new FocusController(objects, controls, {
+        fov: (controls.camera as PerspectiveCamera).fov,
+        offsetAngleInDeg: 10
+      });
+      focusController.focus();
+    }
   };
 
   return <ControlButton icon={<FocusIcon />} onClick={handleObjectFocus} />;
 };
 
-export default ObjectFocus;
+const Observer = observer(ObjectFocus);
+export default Observer;
