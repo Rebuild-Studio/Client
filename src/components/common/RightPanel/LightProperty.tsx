643dded2fdcee5927648c56257eeb50bec6685ae
Jihoo Kim <jihoo94@gmail.com>
2023-10-31 19:30:13 +0900

    feat: 빛 color 조절

--- src/components/common/RightPanel/LightProperty.tsx
+++ src/components/common/RightPanel/LightProperty.tsx
@@ -1,5 +1,7 @@
 import { Light } from 'three';
+import { hsvaToRgbString, rgbaToHsva } from '@uiw/color-convert';
 import styled from 'styled-components';
+import ColorPicker from '@components/common/RightPanel/ColorPicker.tsx';
 import Slider from '@components/common/Slider.tsx';
 
 interface Props {
@@ -7,8 +9,21 @@ interface Props {
 }
 
 const LightProperty = ({ light }: Props) => {
+  const { r, g, b } = light.color;
+  const hsvaColor = rgbaToHsva({ r: r * 255, g: g * 255, b: b * 255, a: 1 });
+
   return (
-    <Wrapper>
+    <div>
+      <ColorWrapper>
+        <span>Light color</span>
+        <ColorPicker
+          label="Light color"
+          color={hsvaColor}
+          onChangeHsvaProp={(hsva) => {
+            light.color.set(hsvaToRgbString(hsva));
+          }}
+        />
+      </ColorWrapper>
       <Slider
         min={0}
         max={50}
@@ -17,10 +32,15 @@ const LightProperty = ({ light }: Props) => {
         onMaterialChange={(value) => (light.intensity = value)}
         title="Intensity"
       />
-    </Wrapper>
+    </div>
   );
 };
 
 export default LightProperty;
 
-const Wrapper = styled.div``;
+const ColorWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 10px;
+`;
