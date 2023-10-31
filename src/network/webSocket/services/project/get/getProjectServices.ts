95a1a373ca4dc6030340f41a174651fc0e2b3a83
junman <junman9595@gmail.com>
2023-10-31 13:34:02 +0900

    feat: pmx 리스트, pmx 불러오기 구현

--- src/network/webSocket/services/project/get/getProjectServices.ts
+++ src/network/webSocket/services/project/get/getProjectServices.ts
@@ -4,6 +4,11 @@ import {
   ResponseGetMxProject
 } from './model/getMxProject.model';
 import { ResponseGetMxProjectList } from './model/getMxProjectList.model';
+import {
+  RequestGetPmxProject,
+  ResponseGetPmxProject
+} from './model/getPmxProject.model';
+import { ResponseGetPmxProjectList } from './model/getPmxProjectList.model';
 
 const getMyMxProjectList = async () => {
   const wsModule = new WsModule({
@@ -26,9 +31,43 @@ const getMxProject = async (params: RequestGetMxProject) => {
   return wsModule.onReceiveMessage<ResponseGetMxProject>();
 };
 
+const getAllPmxProjectList = async () => {
+  const wsModule = new WsModule({
+    targetService: 'FindAllPmxController'
+  });
+  const message = wsModule.assembleMessage();
+  wsModule.sendInChunks(message);
+
+  return wsModule.onReceiveMessage<ResponseGetPmxProjectList>();
+};
+
+const getMyPmxProjectList = async () => {
+  const wsModule = new WsModule({
+    targetService: 'FindMyPmxController'
+  });
+  const message = wsModule.assembleMessage();
+  wsModule.sendInChunks(message);
+
+  return wsModule.onReceiveMessage<ResponseGetPmxProjectList>();
+};
+
+const getPmxProject = async (params: RequestGetPmxProject) => {
+  const wsModule = new WsModule({
+    targetService: 'ReadPmxController'
+  });
+  const { encodedBody } = await wsModule.encodeBody({ ...params });
+  const message = wsModule.assembleMessage(encodedBody);
+  wsModule.send(message);
+
+  return wsModule.onReceiveMessage<ResponseGetPmxProject>();
+};
+
 const getProjectServices = {
   getMyMxProjectList,
-  getMxProject
+  getMxProject,
+  getAllPmxProjectList,
+  getMyPmxProjectList,
+  getPmxProject
 };
 
 export default getProjectServices;
