bd92b596a1280946fdb170c707993906e578e499
Jihoo Kim <jihoo94@gmail.com>
2023-11-06 16:35:54 +0900

    feat: 카메라 mesh 제거 및 카메라 transformation matrix 복원

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import { observer } from 'mobx-react';
 import { MxCanvasCore } from '@mv/core';
 import storeContainer from '@/store/storeContainer';
@@ -7,13 +8,40 @@ import legacyStoreContainer from '../interaction(legacyJS)/src/Components/stores
 const Preview = observer(() => {
   const { projectStore } = storeContainer;
   const { eventSystem_store } = legacyStoreContainer;
-  const sceneJson = projectStore.scene?.toJSON();
+  const sceneForPreview = projectStore.scene?.clone(true);
+
+  if (!sceneForPreview) {
+    return null;
+  }
+
+  const cameraMesh = sceneForPreview.getObjectByName('PREVIEW_CAMERA');
+  if (cameraMesh) {
+    const camera = getCameraFromMesh(cameraMesh);
+    sceneForPreview.add(camera);
+    sceneForPreview.remove(cameraMesh);
+  }
+
+  const sceneJson = sceneForPreview.toJSON();
   const interactionJson = JSON.parse(
     JSON.stringify(eventSystem_store.toJSON())
   );
   const previewJson = createMxJson(sceneJson, interactionJson);
   const data = JSON.stringify(previewJson);
+
   return <MxCanvasCore data={data} mode={'play'} />;
 });
 
 export default Preview;
+
+const getCameraFromMesh = (cameraMesh: THREE.Object3D) => {
+  const camera = cameraMesh.children.find(
+    (child) => child instanceof THREE.Camera
+  ) as THREE.Camera;
+
+  const newCamera = camera.clone();
+  cameraMesh.getWorldPosition(newCamera.position);
+  cameraMesh.getWorldQuaternion(newCamera.quaternion);
+  newCamera.updateMatrix();
+
+  return newCamera;
+};
