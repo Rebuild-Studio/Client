5e28d3b5ec69477b22c02b705c317e67aec6956b
Jihoo Kim <jihoo94@gmail.com>
2023-11-09 14:30:49 +0900

    refactor: 빛, 카메라 mesh 제거하는 로직 createMxJson 함수 내부로

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,58 +1,22 @@
 import { observer } from 'mobx-react';
 import { MxCanvasCore } from '@mv/core';
-import storeContainer from '@/store/storeContainer';
-import { findLightMeshes } from '@/three_components/utils/findLightMeshes.ts';
 import createMxJson from '@/utils/json/createMxJson';
+import projectStore from '@store/project.store.ts';
 import legacyStoreContainer from '../interaction(legacyJS)/src/Components/stores/storeContainer';
 
 const Preview = observer(() => {
-  const { projectStore } = storeContainer;
+  const { scene } = projectStore;
   const { eventSystem_store } = legacyStoreContainer;
-  const sceneForPreview = projectStore.scene;
 
-  if (!sceneForPreview) {
+  if (!scene) {
     return null;
   }
 
-  // restoreCameraTransformation(sceneForPreview);
-
-  const sceneJson = sceneForPreview.toJSON();
+  const sceneJson = scene.toJSON();
   const interactionJson = JSON.parse(
     JSON.stringify(eventSystem_store.toJSON())
   );
   const previewJson = createMxJson(sceneJson, interactionJson);
-
-  // 프리뷰 모드에서 빛 조형물이 보이지 않도록
-  const lightMeshes = findLightMeshes(previewJson.scene.object);
-  lightMeshes.forEach((lightMesh) => {
-    const light = lightMesh.children.find((child) =>
-      child.type.includes('Light')
-    );
-    if (light) {
-      const { matrix } = lightMesh;
-      light.matrix = matrix;
-      previewJson.scene.object.children.push(light);
-      lightMesh.visible = false;
-      lightMesh.children = [];
-    }
-  });
-
-  // 프리뷰 모드에서 카메라 조형물이 보이지 않도록
-  const cameraMesh = previewJson.scene.object.children.find(
-    (child) => child.name === 'PREVIEW_CAMERA'
-  );
-  if (cameraMesh) {
-    const camera = cameraMesh.children.find((child) =>
-      child.type.includes('Camera')
-    );
-    if (camera) {
-      const { matrix } = cameraMesh;
-      camera.matrix = matrix;
-      previewJson.scene.object.children.push(camera);
-      cameraMesh.visible = false;
-      cameraMesh.children = [];
-    }
-  }
 
   const data = JSON.stringify(previewJson);
   return <MxCanvasCore data={data} mode={'play'} />;
--- src/utils/json/createMxJson.ts
+++ src/utils/json/createMxJson.ts
@@ -1,4 +1,5 @@
 import EditableInteraction_VM from '@/interaction(legacyJS)/src/Components/view_models/07. Editable/EditableInteraction_VM';
+import { findLightMeshes } from '@/three_components/utils/findLightMeshes.ts';
 import { MxJson } from '@/types/mxJson/mxJson';
 import { SceneJson } from '@/types/scene/scene';
 import optimizeSceneJson from './optimizeSceneJson';
@@ -7,7 +8,11 @@ const createMxJson = (
   sceneJson: SceneJson,
   interactionJson: object
 ): MxJson => {
+  removeLightMeshes(sceneJson);
+  removeCameraMesh(sceneJson);
+
   optimizeSceneJson(sceneJson);
+
   const editableInteraction = EditableInteraction_VM().toJson(interactionJson);
   const mxJson = {
     scene: sceneJson,
@@ -19,3 +24,39 @@ const createMxJson = (
 };
 
 export default createMxJson;
+
+const removeLightMeshes = (sceneJson: SceneJson) => {
+  const lightMeshes = findLightMeshes(sceneJson.object);
+  lightMeshes.forEach((lightMesh) => {
+    const light = lightMesh.children.find((child) =>
+      child.type.includes('Light')
+    );
+    if (light) {
+      const { matrix } = lightMesh;
+      light.name = lightMesh.name;
+      light.matrix = matrix;
+      sceneJson.object.children.push(light);
+      lightMesh.visible = false;
+      lightMesh.children = [];
+    }
+  });
+};
+
+const removeCameraMesh = (sceneJson: SceneJson) => {
+  const cameraMesh = sceneJson.object.children.find(
+    (child) => child.name === 'PREVIEW_CAMERA'
+  );
+  if (cameraMesh) {
+    const camera = cameraMesh.children.find((child) =>
+      child.type.includes('Camera')
+    );
+    if (camera) {
+      const { matrix } = cameraMesh;
+      camera.name = cameraMesh.name;
+      camera.matrix = matrix;
+      sceneJson.object.children.push(camera);
+      cameraMesh.visible = false;
+      cameraMesh.children = [];
+    }
+  }
+};
